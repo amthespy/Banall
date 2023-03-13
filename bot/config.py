481de3c75bff43314f6d316185ddb57e350c5b40
Spy <119489004+amthespy@users.noreply.github.com>
2023-03-13 15:06:54 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,6 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN=os.environ['6010254886:AAG7R7UcgHBAeQEtgBIe1qB5EL5JmjzPzcU']
-    TELEGRAM_APP_HASH=os.environ['6553ea819bb17098b0e3c62530823328']
-    TELEGRAM_APP_ID=int(os.environ['13750040'])
-    
-    if not TELEGRAM_TOKEN:
         raise ValueError('TELEGRAM BOT TOKEN not set')
     
     if not TELEGRAM_APP_HASH:
